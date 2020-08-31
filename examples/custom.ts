50c90a251806deac8aa3cdc2bf6977eff7ff8566
Delta456 <swstkbaranwal@gmail.com>
2020-08-31 13:53:26 +0530

    examples: update with latest release

--- examples/custom.ts
+++ examples/custom.ts
@@ -1,4 +1,4 @@
-import { badges } from "https://deno.land/x/cli_badges@v0.0.3/index.ts";
+import { badges } from "https://deno.land/x/cli_badges@v0.0.4/index.ts";
 
 console.log(badges("failed", "2", { msgBg: "red" }));
 
--- examples/simple.ts
+++ examples/simple.ts
@@ -1,3 +1,3 @@
-import { badges } from "https://deno.land/x/cli_badges@v0.0.3/index.ts";
+import { badges } from "https://deno.land/x/cli_badges@v0.0.4/index.ts";
 
 console.log(badges("counter", "5"));
